5942ebd36af67ddcb82866b731f0681493358b0c
acopier <filip.hrouda@outlook.cz>
2025-08-23 21:17:42 +0200

    I made the CSS look better, good night.

--- JS/headers.js
+++ JS/headers.js
@@ -1,9 +1,9 @@
 function loadHeaderFooter() {
-  fetch('../HTML/header.html')
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById('top-bar').innerHTML = html;
+  fetch("../HTML/header.html")
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById("top-bar").innerHTML = html;
     })
-    .catch(err => console.error('Header load failed:', err));
+    .catch((err) => console.error("Header load failed:", err));
 }
 loadHeaderFooter();
