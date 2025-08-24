4f13d6c5bd3d04ecd5b811eab7ed85f70bf9d05a
RyanStudioo <yanyan2019ry@gmail.com>
2025-08-24 14:15:09 +0800

    added fadein and slide in animations

--- JS/headers.js
+++ JS/headers.js
@@ -1,11 +1,18 @@
-function loadHeaderFooter(transparent = false) {
+function loadHeaderFooter(transparent = false, animate=false) {
   fetch("../HTML/header.html")
     .then((res) => res.text())
     .then((html) => {
       document.getElementById("top-bar").innerHTML = html;
       if (transparent) {
-      const nav = document.querySelector('nav'); // or 'nav' or '#top-bar'
+      const nav = document.querySelector('nav'); 
       if (nav) nav.classList.add('transparent');}
+      if (animate) {
+        const nav = document.querySelector('nav'); 
+        if (nav) nav.classList.add('slide-in');
+      }
+    })
+    .then(() => {
+    document.body.style.animation = "fadeIn 1s forwards";
     })
     .catch((err) => console.error("Header load failed:", err));
     }
