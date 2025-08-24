7cc666a239d9b30169467eef809d76fccb260781
RyanStudioo <yanyan2019ry@gmail.com>
2025-08-24 12:49:02 +0800

    repository page

--- JS/headers.js
+++ JS/headers.js
@@ -1,9 +1,12 @@
-function loadHeaderFooter() {
+function loadHeaderFooter(transparent = false) {
   fetch("../HTML/header.html")
     .then((res) => res.text())
     .then((html) => {
       document.getElementById("top-bar").innerHTML = html;
+      if (transparent) {
+      const nav = document.querySelector('nav'); // or 'nav' or '#top-bar'
+      if (nav) nav.classList.add('transparent');}
     })
     .catch((err) => console.error("Header load failed:", err));
-}
-loadHeaderFooter();
+    }
+
